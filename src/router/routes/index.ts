f57e6f7a6b25601a65661407f594f755a4183436
cjyiz <cjyiz@aliyun.com>
2025-03-17 20:40:30 +0800

    新增登录注册页面

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,8 +2,12 @@
 
 import HelloWorld from '@/components/HelloWorld.vue';
 import TheWelcome from '@/components/TheWelcome.vue';
+import Login from '@/views/login/login.vue';
+import Register from '@/views/login/register.vue';
 
 export const routes = [
   { path: '/', component: HelloWorld },
   { path: '/about', component: TheWelcome },
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
 ];
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>面板内容</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
