6898ae493279ce325d4ccb152234b2f437fa1f15
cjyiz <cjyiz@aliyun.com>
2025-03-28 16:18:25 +0800

    按需导入引入element-plus

---
+++ src/login/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const loginRules = computed(() => {
+  return {
+    username: [
+      {
+        required: true,
+        trigger: 'blur',
+        message: '请输入用户名',
+      },
+    ],
+    password: [
+      {
+        required: true,
+        trigger: 'blur',
+        message: '请输入密码',
+      },
+      {
+        min: 6,
+        message: '密码最少为6位',
+        trigger: 'blur',
+      },
+    ],
+    captchaCode: [
+      {
+        required: true,
+        trigger: 'blur',
+        message: '请输入验证码',
+      },
+    ],
+  }
+})
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,26 @@ import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd())
   console.log('cjyizVIte配置', env)
   return {
-    plugins: [vue(), vueJsx(), vueDevTools()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
